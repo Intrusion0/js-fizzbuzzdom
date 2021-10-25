699270b2a1b8bea45363c53a62a9cfbe3a9795b8
Mario <lombardomario01@gmail.com>
2021-10-25 16:29:39 +0200

    Created bonus

---
+++ bonus/js/script.js
@@ -0,0 +1,28 @@
+/* Scrivi un programma che esegua un ciclo da 1 a 100
+e a ogni iterazione appenda un elemento html al container con uno stile differente a seconda del valore dell’indice
+per i multipli di 3,
+per i multipli di 5
+e per i valori che sono sia multipli di 3 che di 5.
+*/
+
+const contSquare = document.querySelector(".containerSquare");
+
+for (let i = 1; i <= 100; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    div.append(i);
+    contSquare.append(div);
+
+    if (i % 15 === 0) { // --OPPURE-- (i % 3 === 0 && i % 5 === 0)
+        div.classList.add("fizzbuzz");
+        div.innerHTML = `fizzbuzz`;
+    }
+    else if (i % 3 === 0) {
+        div.classList.add("fizz");
+        div.innerHTML = `fizz`;
+    }
+    else if (i % 5 === 0) {
+        div.classList.add("buzz");
+        div.innerHTML = `buzz`;
+    }
+}
